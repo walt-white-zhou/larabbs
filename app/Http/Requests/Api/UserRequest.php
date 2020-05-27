3efe330fb708652621b7c58f0140ef1523e66c63
root <root@DESKTOP-6RO2RCV.localdomain>
2020-05-27 17:20:40 +0800

    weapp users store

--- app/Http/Requests/Api/UserRequest.php
+++ app/Http/Requests/Api/UserRequest.php
@@ -12,25 +12,26 @@ class UserRequest extends FormRequest
      */
     public function rules()
     {
-        switch($this->method()) {
-        case 'POST':
-            return [
-                'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name',
-                'password' => 'required|string|min:6',
-                'verification_key' => 'required|string',
-                'verification_code' => 'required|string',
-            ];
-            break;
-        case 'PATCH':
-            $userId = auth('api')->id();
-
-            return [
-                'name' => 'between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' .$userId,
-                'email'=>'email|unique:users,email,'.$userId,
-                'introduction' => 'max:80',
-                'avatar_image_id' => 'exists:images,id,type,avatar,user_id,'.$userId,
-            ];
-            break;
+        switch ($this->method()) {
+            case 'POST':
+                return [
+                    'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name',
+                    'password' => 'required|string|min:6',
+                    'verification_key' => 'required|string',
+                    'verification_code' => 'required|string',
+                ];
+                break;
+            case 'PUT':
+                break;
+            case 'PATCH':
+                $userId = \Auth::guard('api')->id();
+                return [
+                    'name' => 'between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . $userId,
+                    'email' => 'email',
+                    'introduction' => 'max:80',
+                    'avatar_image_id' => 'exists:images,id,type,avatar,user_id,' . $userId,
+                ];
+                break;
         }
     }
 
